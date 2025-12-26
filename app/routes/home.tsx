2fcb8d6921433f4d6440e3f8485a52990f28b978
Can Boz <canboz3460@gmail.com>
2025-12-26 16:39:45 +0300

    first commit

---
+++ app/routes/home.tsx
@@ -0,0 +1,19 @@
+import type { Route } from "./+types/home";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Resumind" },
+    { name: "description", content: "Smart Feedback For Your Dream Job!" },
+  ];
+}
+
+export default function Home(){
+  return <main>
+    <section className="main-section">
+      <div className="page-heading">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h2>Review your submissions and check AI-powered feedback.</h2>
+      </div>
+    </section>
+  </main>
+}
